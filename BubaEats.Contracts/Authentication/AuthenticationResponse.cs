42f214102d26617b1743f43c12150a966aa8fa3e
Marcin Pilarczyk <marcinpilarczyk@me.com>
2024-05-21 14:27:52 +0200

    Add contract response and requests

---
+++ BubaEats.Contracts/Authentication/AuthenticationResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace BubaEats.Contracts.Authentication;
+
+public record AuthenticationResponse(
+    Guid Id,
+    string FirstName,
+    string LastName,
+    string Email,
+    string Password,
+    string Token)
+{ }
---
+++ BubaEats.Contracts/Authentication/LoginRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace BubaEats.Contracts.Authentication;
+
+public record LoginRequest(string Email, string Password) { }
