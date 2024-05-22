15c4c64264ecf4a2d16c5b60ef4364e9225b99ba
Marcin Pilarczyk <marcinpilarczyk@me.com>
2024-05-22 08:30:23 +0200

    Wire up interfaces and implementations

--- BubaEats.Application/Services/Authentication/AuthenticationService.cs
+++ BubaEats.Application/Services/Authentication/AuthenticationService.cs
@@ -1,14 +1,43 @@
-﻿namespace BubaEats.Application;
+﻿using BubaEats.Application.Common.Interfaces.Authentication;
+
+namespace BubaEats.Application;
 
 public class AuthenticationService : IAuthenticationService
 {
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
+
     public AuthenticationResult Login(string email, string password)
     {
-        return new AuthenticationResult(Guid.NewGuid(), "John", "Doe", email, password, "1234");
+        return new AuthenticationResult(
+            Guid.NewGuid(),
+            "John",
+            "Doe",
+            email,
+            password,
+            "1234");
     }
 
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
-        return new AuthenticationResult(Guid.NewGuid(), firstName, lastName, email, password, "1234");
+        // Check if user exists
+
+        // Create user (generate unique ID)
+        var userId = Guid.NewGuid();
+
+        // Create JWT Token
+        var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+
+        return new AuthenticationResult(
+            userId,
+            firstName,
+            lastName,
+            email,
+            password,
+            token);
     }
 }
