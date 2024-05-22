a06f43c3160861fbd1e4b4d048374435b065ea39
Marcin Pilarczyk <marcinpilarczyk@me.com>
2024-05-22 08:29:40 +0200

    Add JwtTokenGenerator

---
+++ BubaEats.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -0,0 +1,42 @@
+﻿using BubaEats.Application;
+using BubaEats.Application.Common.Interfaces.Authentication;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BubaEats.Infrastructure;
+
+public class JwtTokenGenerator : IJwtTokenGenerator
+{
+    private readonly IDateTimeProvider _dateTimeProvider;
+
+    public JwtTokenGenerator(IDateTimeProvider dateTimeProvider)
+    {
+        _dateTimeProvider = dateTimeProvider;
+    }
+
+    public string GenerateToken(Guid userId, string firstName, string lastName)
+    {
+        var signingCredentials = new SigningCredentials(
+            new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("super-duper-secret-password-longer-then-expected")),
+            SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+            new Claim(JwtRegisteredClaimNames.GivenName, firstName),
+            new Claim(JwtRegisteredClaimNames.FamilyName, lastName),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var securityToken = new JwtSecurityToken(
+            issuer: "BubaEats",
+            expires: _dateTimeProvider.UtcNow.AddMinutes(60),
+            claims: claims,
+            signingCredentials: signingCredentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(securityToken);
+    }
+}
