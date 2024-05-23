f93331f22bfe367d71964d612f5125a6ff7bd462
Marcin Pilarczyk <marcinpilarczyk@me.com>
2024-05-23 20:27:40 +0200

    Create inmemory implementation of user repository

---
+++ BubaEats.Infrastructure/Persistent/InMemoryUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using BubaEats.Application.Common.Interfaces.Persistent;
+using BubaEats.Domain.Entities;
+
+namespace BubaEats.Infrastructure.Persistent;
+
+public class InMemoryUserRepository : IUserRepository
+{
+    private static readonly List<User> _users = [];
+    public void Add(User user)
+    {
+        _users.Add(user);
+    }
+
+    public User? GetUserByEmail(string email)
+    {
+        return _users.SingleOrDefault(user => user.Email == email);
+    }
+}
