66c512cddbe77c698b6cc6bfbf48b96ad15a8eb6
Marcin Pilarczyk <marcinpilarczyk@me.com>
2024-05-28 12:21:26 +0200

    Add ErrorHandlingFilterAttribute

--- BubaEats.Api/Program.cs
+++ BubaEats.Api/Program.cs
@@ -1,4 +1,4 @@
-using BubaEats.Api;
+using BubaEats.Api.Filter;
 using BubaEats.Application;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,12 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddApplication()
                     .AddInfrastructure(builder.Configuration);
 
-    builder.Services.AddControllers();
+    builder.Services.AddControllers(
+        options => options.Filters.Add<ErrorHandlingFilterAttribute>() // Global error filter. You can use it only for one controller or even route.
+        );
 }
 
 var app = builder.Build();
 {
-    app.UseMiddleware<ErrorHandlingMiddleware>();
+    // app.UseMiddleware<ErrorHandlingMiddleware>();
     app.UseHttpsRedirection();
     app.MapControllers();
 
