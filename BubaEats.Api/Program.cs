144deb6acb959c6837ec288a0fbe77dca3789c8f
Marcin Pilarczyk <marcinpilarczyk@me.com>
2024-05-28 12:50:00 +0200

    Controller error handling

--- BubaEats.Api/Program.cs
+++ BubaEats.Api/Program.cs
@@ -1,4 +1,3 @@
-using BubaEats.Api.Filter;
 using BubaEats.Application;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,14 +5,16 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddApplication()
                     .AddInfrastructure(builder.Configuration);
 
-    builder.Services.AddControllers(
-        options => options.Filters.Add<ErrorHandlingFilterAttribute>() // Global error filter. You can use it only for one controller or even route.
-        );
+    // builder.Services.AddControllers(
+    //     options => options.Filters.Add<ErrorHandlingFilterAttribute>() // Global error filter. You can use it only for one controller or even route.
+    //     );
+    builder.Services.AddControllers();
 }
 
 var app = builder.Build();
 {
     // app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
 
