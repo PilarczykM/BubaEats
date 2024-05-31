99966176f3bdcff1b390bf136d9b7b2b51a216f6
Marcin Pilarczyk <marcinpilarczyk@me.com>
2024-05-31 09:27:59 +0200

    Add mapping extension

---
+++ BubaEats.Api/Common/Mapping/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Mapster;
+using System.Reflection;
+
+namespace BubaEats.Api.Common.Mapping;
+
+public static class Extensions
+{
+    public static IServiceCollection AddMapping(this IServiceCollection services)
+    {
+        var config = TypeAdapterConfig.GlobalSettings;
+        config.Scan(Assembly.GetExecutingAssembly());
+
+        services.AddSingleton(config);
+        services.AddMapster();
+        return services;
+    }
+}
