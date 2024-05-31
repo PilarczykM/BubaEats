29bcc35ab3f0c1344d1ad9a4d7fd48731bf3a806
Marcin Pilarczyk <marcinpilarczyk@me.com>
2024-05-31 14:16:31 +0200

    Wire up dependency injection

--- BubaEats.Application/Extensions.cs
+++ BubaEats.Application/Extensions.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BubaEats.Application.Authentication.Command.Register;
+using BubaEats.Application.Authentication.Common;
+using BubaEats.Application.Common.Behaviors;
+using ErrorOr;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace BubaEats.Application;
@@ -8,6 +14,10 @@ public static class Extensions
     public static IServiceCollection AddApplication(this IServiceCollection service)
     {
         service.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        service.AddScoped<
+            IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>,
+            ValidationRegisterCommandBehavior>();
+        service.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         return service;
     }
 
