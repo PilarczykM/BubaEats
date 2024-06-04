d74c72b3748a330c78aa6a4ddc70fd2ed9b1a84b
Marcin Pilarczyk <marcinpilarczyk@me.com>
2024-06-04 15:13:30 +0200

    Add

--- BubaEats.Api/Controllers/ApiController.cs
+++ BubaEats.Api/Controllers/ApiController.cs
@@ -1,11 +1,13 @@
 ﻿using BubaEats.Api.Common.Http;
 using ErrorOr;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace BubaEats.Api.Controllers;
 
 [ApiController]
+[Authorize]
 public class ApiController : ControllerBase
 {
     protected IActionResult Problem(List<Error> errors)
--- BubaEats.Infrastructure/Extensions.cs
+++ BubaEats.Infrastructure/Extensions.cs
@@ -4,8 +4,12 @@ using BubaEats.Application.Common.Interfaces.Services;
 using BubaEats.Infrastructure.Authentication;
 using BubaEats.Infrastructure.Persistent;
 using BubaEats.Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace BubaEats.Application;
 
@@ -14,9 +18,36 @@ public static class Extensions
     public static IServiceCollection AddInfrastructure(
         this IServiceCollection service,
         ConfigurationManager configuration)
-        => service
-        .Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName))
-        .AddSingleton<IDateTimeProvider, DateTimeProvider>()
-        .AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>()
-        .AddScoped<IUserRepository, InMemoryUserRepository>();
+    {
+        service.AddAuth(configuration);
+        service.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+        service.AddScoped<IUserRepository, InMemoryUserRepository>();
+
+        return service;
+    }
+
+    public static IServiceCollection AddAuth(
+    this IServiceCollection service,
+    ConfigurationManager configuration)
+    {
+        var jwtSettings = new JwtSettings();
+        configuration.Bind(JwtSettings.SectionName, jwtSettings);
+        service.AddSingleton(Options.Create(jwtSettings));
+        service.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+
+        service.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings.Issuer,
+                ValidAudience = jwtSettings.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret))
+            });
+
+        return service;
+    }
+
 }
